7003ca63c3d1f7767b0747e14ef87130987c240c
Andri Rafti <andrirafti@Andris-MacBook-Air.local>
2021-04-29 17:15:31 -0400

    update

--- client/gyms/src/services/api-config.js
+++ client/gyms/src/services/api-config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 //Since we make axios.create into api we will be doing
 //localhost:3000/api/________
 // to call stuff 
-const baseURL=process.env.NODE_ENV === 'production' ? /* link to your heroku app. Example:*/'https://git.heroku.com/gym-trainer-api.git' : 'http://localhost:3000'
+const baseURL=process.env.NODE_ENV === 'production' ? /* link to your heroku app. Example:*/'https://gym-trainer-api.herokuapp.com/' : 'http://localhost:3000'
 const api = axios.create({
   baseURL: baseURL
 })
