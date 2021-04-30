4676b5fc4218d2fa13fadbebdf8128d933438803
Andri Rafti <andrirafti@Andris-MacBook-Air.local>
2021-04-30 07:55:00 -0400

    update

--- client/gyms/src/services/api-config.js
+++ client/gyms/src/services/api-config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 //originally keep baseURL=localhost:3000
 //make sure to grab the heroku.com link not heroku.api 
-const baseURL=process.env.NODE_ENV === 'production' ? /* link to your heroku app. Example:*/'https://gym-trainer-api.herokuapp.com/' : 'http://localhost:3001'
+const baseURL=process.env.NODE_ENV === 'production' ? 'https://gym-trainer-api.herokuapp.com/' : 'http://localhost:3001'
 const api = axios.create({
   baseURL: baseURL
 })
